bff84f62aabd70b481a79db9761412bbf443fbb4
Anilton Viganigo Junior <viganigojr@gmail.com>
2020-06-17 22:35:42 -0300

    Modificação: usando o comando remove

--- src/application/Program.java
+++ src/application/Program.java
@@ -13,7 +13,7 @@ public class Program {
 		set.add("Notebook");
 		set.add("Tablet");
 		
-		System.out.println(set.contains("Notebook"));
+		set.remove("Tablet");
 		
 		for (String p : set) {
 			System.out.println(p);
